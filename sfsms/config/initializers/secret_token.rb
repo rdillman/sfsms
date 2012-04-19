851a196e55494a6053f69684afec98f414603dea
Dian Rosanti <dian.rosanti@gmail.com>
2012-04-19 16:21:57 -0700

    rails

---
+++ sfsms/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sfsms::Application.config.secret_token = '998285a80a1dfef83fcc224987549b0ca6b5e828e80137bebe659ccd1405556410648331012a5fcc15d675388aaf60e80753953c2db81c4ce17794a8c66fcdfb'
---
+++ sfsms/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sfsms::Application.config.session_store :cookie_store, :key => '_sfsms_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sfsms::Application.config.session_store :active_record_store
